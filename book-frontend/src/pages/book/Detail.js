ebe1441fb0d250b04713b8e2b56604a8741d3f7b
unknown <doaram0610@gmail.com>
2022-03-25 16:13:58 +0900

    프론트앤드 커밋

---
+++ book-frontend/src/pages/book/Detail.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useParams, useNavigate } from 'react-router-dom'; //react-router-dom버전6 이후부터 props.history.push 사용 못한다
+
+const Detail = () => {
+  const params = useParams(); //그냥 useParams()로 받으면 오브젝트이다.
+  const navigate = useNavigate();
+  const [book, setBook] = useState({
+    id: '',
+    title: '',
+    author: '',
+  });
+
+  console.log('detail', params);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/book/' + params.id)
+      .then((res) => res.json())
+      .then((res) => {
+        setBook(res);
+      });
+  }, []);
+
+  const deleteBook = () => {
+    fetch('http://localhost:8080/book/' + params.id, {
+      method: 'DELETE',
+    })
+      .then((res) => res.text())
+      .then((res) => {
+        if (res === 'ok') {
+          //props.history.push('/'); //요거 안먹는데 라우터 v6 이후부터 아래처럼 해야됨
+          navigate('/');
+        } else {
+          alert('삭제실패');
+        }
+      });
+  };
+
+  const updateBook = () => {
+    // props.history.push('/updateForm/' + id);
+    navigate('/updateForm/' + params.id);
+  };
+
+  return (
+    <div>
+      <h1>책 상세보기</h1>
+      <Button variant="warning" onClick={updateBook}>
+        수정
+      </Button>{' '}
+      <Button variant="danger" onClick={deleteBook}>
+        삭제
+      </Button>
+      <hr />
+      <h3>{book.author}</h3>
+      <h1>{book.title}</h1>
+    </div>
+  );
+};
+
+export default Detail;
---
+++ book-frontend/src/pages/book/UpdateForm.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const UpdateForm = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+
+  //아래 input 값에 입력된 값을 보관하는 상수
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  //백앤드 api 호출
+  useEffect(() => {
+    fetch('http://localhost:8080/book/' + params.id)
+      .then((res) => res.json())
+      .then((res) => {
+        setBook(res); //받은 결과를 저장
+      });
+  }, []);
+
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault(); //submit 이 action을 안타고 자기 할일을 그만함
+    fetch('http://localhost:8080/book/' + params.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(book), //json 객체로 만든다
+    })
+      .then((res) => {
+        console.log(1, res);
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        console.log(2, res);
+        if (res !== null) {
+          navigate('/book/' + params.id); //v6 이하버전에서는 props.history 를 썼다
+        } else {
+          alert('책 등록에 실패하였습니다.');
+        }
+      }); //catch 는 여기서 오류나야 실행된다
+  };
+
+  return (
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Title"
+          onChange={changeValue}
+          name="title"
+          value={book.title}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+          value={book.author}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        수정
+      </Button>
+    </Form>
+  );
+};
+
+export default UpdateForm;
