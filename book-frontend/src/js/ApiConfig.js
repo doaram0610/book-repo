f2f45a4aed815e4e3713d1d649fc4c5da37aeac7
unknown <doaram0610@gmail.com>
2022-04-29 14:42:11 +0900

    변경 커밋

--- book-frontend/src/js/ApiConfig.js
+++ book-frontend/src/js/ApiConfig.js
@@ -1,3 +1,3 @@
 // global_variables.js
-export const API_BASE_URL = 'http://localhost:5000';
-// export const API_BASE_URL = 'http://3.36.25.145:5080';
+//export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'http://52.79.180.99:5000';
